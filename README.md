898c0191f4e0692f5bb49809fca04c6ae7e63301
Ando Yasushi <andyjpn@gmail.com>
2014-06-03 00:19:36 +0900

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+# Amber Slide
+
+The Application or The Library doing The Thing.
+
+## Getting Started
+
+Installing dependencies:
+
+```
+npm install
+```
+
+Open `index.html` in your browser and follow the instructions
+
---
+++ src/AmberSlide-Tests.js
@@ -0,0 +1,7 @@
+define("amber_core/AmberSlide-Tests", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "amber_vm/globals", "amber_core/SUnit"], function(smalltalk,nil,_st, globals){
+smalltalk.addPackage('AmberSlide-Tests');
+smalltalk.packages["AmberSlide-Tests"].transport = {"type":"amd","amdNamespace":"amber_core"};
+
+smalltalk.addClass('AmberSlideTest', globals.TestCase, [], 'AmberSlide-Tests');
+
+});
